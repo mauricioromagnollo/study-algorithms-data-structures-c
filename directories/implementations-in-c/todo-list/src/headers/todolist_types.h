7550b33f79438e37c5bbf6307aa37fe1db05b640
x0n4d0 <romagnolimauricio@gmail.com>
2020-01-25 23:47:33 -0300

    feat: add linked list in todo_list_types.h

--- directories/implementations-in-c/todo-list/src/headers/todolist_types.h
+++ directories/implementations-in-c/todo-list/src/headers/todolist_types.h
@@ -9,25 +9,43 @@ typedef char name_t[15];
 typedef char date_t[12];
 typedef unsigned int position_t, id_t;
 
-/**
- * Tipo Tarefa
- */
 typedef struct {
   id_t id;
   description_t description;
   date_t dateInit;
   date_t dateEnd;
-  bool isDone; // estado
-} task_t;
+  bool isDone;
+}task_t;
 
-/**
- * Tipo Lista de Tarefas
- */
+typedef struct node_task_list *task_list_ptr_t;
+
+typedef struct node_task_list {
+  task_t task;
+  task_list_ptr_t next;
+}node_task_list_t;
+
+typedef struct {
+  node_task_list_t first, last;
+}task_list_t;
+
 typedef struct {
   name_t name;
   description_t description;
-  task_t *tasks;
-} list_t;
+  task_list_t task_list;
+}todolist_t;
 
+typedef struct node_todo_list *node_todo_list_ptr_t;
+
+typedef struct node_todo_list {
+  todolist_t todolist;
+  node_todo_list_ptr_t next;
+}node_todo_list_t;
+
+typedef struct {
+  node_todo_list_t first, last;
+}todolists_t;
+
+#define TASK_LIST_NODE_SIZE sizeof(node_task_list_t)
+#define TODO_LIST_NODE_SIZE sizeof(node_todo_list_t)
 
 #endif // TODOLIST_TYPES_H_INCLUDED
